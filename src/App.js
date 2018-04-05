2263593a4e8b8a07db9ddf8efe9f304a0b0e0a41
Josiah Sprague <josiah.sprague@gmail.com>
2018-04-05 13:21:20 -0400

    Render features and subfeatures

--- src/App.js
+++ src/App.js
@@ -4,11 +4,31 @@ import features from './features';
 
 class App extends Component {
   render() {
+    const Feature = props => {
+      const { title, presence, subfeatures } = props.feature;
+      return (
+        <li>
+          <b>{title}</b>: {presence ? 'Yes' : 'No'}
+          {subfeatures ? (
+            <ul>
+              {subfeatures.map(feature => (
+                <Feature key={feature.title} feature={feature} />
+              ))}
+            </ul>
+          ) : null}
+        </li>
+      );
+    };
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Hipcamp Sample</h1>
         </header>
+        <ul>
+          {features.map(feature => (
+            <Feature key={feature.title} feature={feature} />
+          ))}
+        </ul>
         <div className="App-intro">
           <pre>{JSON.stringify(features)}</pre>
         </div>
